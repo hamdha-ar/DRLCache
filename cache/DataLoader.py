9eee6e7fe7562866ff0333bbe4e00e1161415ab3
hamdha-ar <134587773+hamdha-ar@users.noreply.github.com>
2025-02-19 18:12:49 +0530

    Update DataLoader.py

--- cache/DataLoader.py
+++ cache/DataLoader.py
@@ -2,6 +2,7 @@ import sys, os, random
 import numpy as np
 import pandas as pd
 
+#This is a base class (parent) that provides a template for specific data loaders.
 class DataLoader(object):
     def __init__(self):
         self.requests = []
@@ -13,12 +14,15 @@ class DataLoader(object):
         pass
 
 class DataLoaderPintos(DataLoader):
+    #progs: List of CSV files (real disk logs).
+    #boot=False: If True, includes all data; otherwise, filters only executed requests.
     def __init__(self, progs, boot=False):
         super(DataLoaderPintos, self).__init__()
 
         if isinstance(progs, str): progs = [progs]
         for prog in progs:
             df = pd.read_csv(prog, header=0)
+            #filtering only the executed requests.
             if not boot: df = df.loc[df['boot/exec'] == 1, :]
             self.requests += list(df['blocksector'])
             self.operations += list(df['read/write'])
