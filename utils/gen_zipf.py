6f9c233980e035e53c9c28e342ad01c68d65b3ec
hamdha-ar <134587773+hamdha-ar@users.noreply.github.com>
2025-02-19 17:19:08 +0530

    Update gen_zipf.py

--- utils/gen_zipf.py
+++ utils/gen_zipf.py
@@ -7,9 +7,13 @@ if __name__ == "__main__":
         print("Usage: %s <save_path> <num_resources> <num_samples> <zipf_param> <num_progs>" % sys.argv[0])
         exit(0)
     save_path = sys.argv[1]
+    #total number of files that need to be created,resources
     num_files = int(sys.argv[2])
+    #total number of requests that needs to be simulated
     num_samples = int(sys.argv[3])
+    #How uneven the popularity is. Higher = more unfair ie some files will be having high popularity
     param = float(sys.argv[4])
+    # no of users making requests
     num_progs = int(sys.argv[5])
 
     df = None
@@ -18,11 +22,13 @@ if __name__ == "__main__":
         # Random ranks. Note that it starts from 1.
         ranks = np.random.permutation(files) + 1
 
-        # Distribution
+        # Distribution,Assign Popularity Using Zipf's Law
+        # files with lower ranks ,gets higher popularity and bigger ranks (rank 100) get lower probability.
         pdf = 1 / np.power(ranks, param)
         pdf /= np.sum(pdf)
 
         # Draw samples
+        #Pick files Based on Popularity
         requests = np.random.choice(files, size=num_samples, p=pdf)
         operations = np.full_like(requests, 0)
         executions = np.full_like(requests, 1)
